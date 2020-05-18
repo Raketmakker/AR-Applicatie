0ff12cce14ec915c437ffc22afe43d740256bb4d
Jasper <j.bonefaas@student.avans.nl>
2020-05-18 15:07:44 +0100

    Added GameObject and several Component classes

---
+++ TI2.4-CGR-Template/Texture.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Texture.h"
+#include "stb_image.h"
+#include <iostream>
+
+Texture::Texture(const std::string& fileName)
+{
+    //TODO
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, comp;
+    unsigned char* data = stbi_load(fileName.c_str(), &width, &height, &comp, 4);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    stbi_image_free(data);
+}
+
+void Texture::bind()
+{
+}
--- TI2.4-CGR-Template/main.cpp
+++ TI2.4-CGR-Template/main.cpp
@@ -7,6 +7,7 @@
 #include "opencv2/imgproc/imgproc.hpp" 
 #include "opencv2/highgui/highgui.hpp"
 #include "blobdetectionavans.h"
+#define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 #include <GL/glew.h>
@@ -76,10 +77,4 @@ void draw()
     glClearColor(0.3f, 0.4f, 0.6f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     
-
-    tigl::begin(GL_TRIANGLES);
-    tigl::addVertex(Vertex::PC(glm::vec3(-1, 0, 0), glm::vec4(1, 0, 0, 1)));
-    tigl::addVertex(Vertex::PC(glm::vec3(1, 0, 0), glm::vec4(0, 1, 0, 1)));
-    tigl::addVertex(Vertex::PC(glm::vec3(0, 1, 0), glm::vec4(0, 0, 1, 1)));
-    tigl::end();
 }
